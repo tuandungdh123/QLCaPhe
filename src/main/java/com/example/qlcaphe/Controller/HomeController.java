fe237f53519ce44fbea0ec3dc233d815fbfd1271
Truong Tuan Dung <tuandungtruong122@gmai.com>
2024-07-21 16:30:29 +0700

    Giao Dien

--- src/main/java/com/example/qlcaphe/Controller/HomeController.java
+++ src/main/java/com/example/qlcaphe/Controller/HomeController.java
@@ -2,12 +2,32 @@ package com.example.qlcaphe.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class HomeController {
     @GetMapping({"/login","/"})
     public String getLogin() {
         return "Login";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return "Pages/Home/admin";
+    }
+
+    @GetMapping("/productManagement")
+    public String getProductManagement(){
+        return "Pages/Home/OrderManagement";
+    }
+
+    @GetMapping("/staffManagement")
+    public String getStaffManagement(){
+        return "Pages/Home/Staff";
+    }
+
+    @GetMapping("/admin/oderManagement")
+    public String getOderManagement(){
+        return "Pages/Home/OrderManagement";
     }
 }
